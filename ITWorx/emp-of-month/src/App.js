8c6e55f43dfcf1ce392de828bd90fae9ad049bda
NoureldinAbdelalim <noureldin.abdelalim@student.guc.edu.eg>
2024-08-12 14:31:31 +0300

    update

--- ITWorx/emp-of-month/src/App.js
+++ ITWorx/emp-of-month/src/App.js
@@ -8,15 +8,11 @@ import employee3 from './employee3.jpg';
 import logo from './logo.jpeg';
 import NavAdmin from './components/navadmin';
 import SideNavAdmin from './components/sidenavadmin';
-<<<<<<< Updated upstream
-import Adminnnnn from './adminnnnnn'; // Import the Adminnnnn component
 import VoteEmp from './voteemp'; // Import the VoteEmp component
 import Resultss from './results';
-=======
 
 import Adminnnnn from './adminHome'; // Import the Adminnnnn component
 import Login from './login';
->>>>>>> Stashed changes
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
@@ -24,16 +20,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-<<<<<<< Updated upstream
         <Route path="/admin" exact element={<Adminnnnn />} />
         <Route path="/voteemp" exact element={<VoteEmp />} />
         <Route path="/results" exact element={<Resultss />} />
-=======
-        <Route path="/admin" exact element={<Adminnnnn />}/>
           
         <Route path="/login" exact element={<Login />}/>
         
->>>>>>> Stashed changes
       </Routes>
     </BrowserRouter>
   );
